7238476e6588c50aa33709ed615c9b38c6e05451
aramdov <aramdov444@gmail.com>
2020-12-02 22:24:14 -0800

    FAB and sub-menu in DailyActivity

--- app/src/main/java/com/example/smartcalendar/DailyActivity.java
+++ app/src/main/java/com/example/smartcalendar/DailyActivity.java
@@ -3,12 +3,101 @@ package com.example.smartcalendar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 public class DailyActivity extends AppCompatActivity {
+
+    // declaring variables for FAB and its sub-menu. Animations as well. TextView sits next to each FAB in the sub-menu
+    private FloatingActionButton fab_main, fab_camera, fab_event;
+    private Animation fab_open, fab_close, fab_clock, fab_anticlock;
+    TextView Event_text, Camera_text;
+
+    // boolean trigger for the fab_main.setOnClickListener(line 48)
+    Boolean isOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily);
+        //public void add (R.layout.activity_daily);
+        // setContentView(R.layout.activity_daily);
+
+
+        // finding views from layout_fab_submenu and animations from the res/anim/ folder.
+        fab_main = findViewById(R.id.fab_main);
+        fab_camera = findViewById(R.id.fab_camera);
+        fab_event = findViewById(R.id.fab_event);
+
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_clock = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_rotate_clock);
+        fab_anticlock = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_rotate_anticlock);
+
+        Event_text = (TextView) findViewById(R.id.Event_text);
+        Camera_text = (TextView) findViewById(R.id.Camera_text);
+
+        // main FAB button click listener. Triggering this will pop-up the menus based on the boolean defined above (line 23)
+        fab_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (isOpen) {
+
+                    Event_text.setVisibility(View.INVISIBLE);
+                    Camera_text.setVisibility(View.INVISIBLE);
+                    fab_camera.startAnimation(fab_close);
+                    fab_event.startAnimation(fab_close);
+                    fab_main.startAnimation(fab_anticlock);
+                    fab_camera.setClickable(false);
+                    fab_event.setClickable(false);
+                    isOpen = false;
+
+                } else {
+                    Event_text.setVisibility(View.VISIBLE);
+                    Camera_text.setVisibility(View.VISIBLE);
+                    fab_camera.startAnimation(fab_open);
+                    fab_event.startAnimation(fab_open);
+                    fab_main.startAnimation(fab_clock);
+                    fab_camera.setClickable(true);
+                    fab_event.setClickable(true);
+                    isOpen = true;
+                }
+            }
+        });
+
+        // camera button click listener
+        // TODO: 1. Setup implicit intent to access camera and take a photograph.
+        // TODO:        Startactivity for result? How to navigate from here? Should there be a camera layout screen or jump straight to device camera?
+        // TODO: 2. TextRecognition processor on high quality image then -> auto-fill event details with Natty parser and other support libs
+        fab_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getApplicationContext(), "Take picture", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        // on event add button
+        fab_event.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getApplicationContext(), "Add event", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
+
+
+
 }
